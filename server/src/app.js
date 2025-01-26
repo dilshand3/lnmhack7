58bbb4ec4f5d60e57308fc15f890f3651dd213cf
dilshan.d3 <dilshan72404@gmail.com>
2025-01-26 17:25:29 +0530

    / route added

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config({
-    path: "./.env"
+  path: "./.env"
 });
 const app = express();
 
@@ -12,12 +12,14 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: (origin, callback) => {
-      callback(null, true);
-    },
-    credentials: true,
-  }));
-
+  origin: (origin, callback) => {
+    callback(null, true);
+  },
+  credentials: true,
+}));
+app.get("/", (req, res) => {
+  res.send("hello lnm i am ready")
+})
 import userRouter from "./routes/user.route.js";
 app.use("/api/user", userRouter);
 
